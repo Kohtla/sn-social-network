b741aca76b3292d63cc65195093fec0e9d763279
Alex Klimakov <MKmoolooo@yandex.ru>
2020-09-28 22:07:25 +0400

    Like functionality

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostView
+from .views import PostView, LikeView
 
 app_name = 'posts'
 
 
 urlpatterns = [
     path('', PostView.as_view()),
+    path('/like/<int:post>', LikeView.as_view())
 ]
--- posts/views/like.py
+++ posts/views/like.py
@@ -3,14 +3,30 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from posts.serializers import LikeSerializer
+from posts.models import Post, Like
 
 
 class LikeView(APIView):
     permission_classes = (IsAuthenticated, )
 
-    def post(self, request):
-        pass
+    def post(self, request, **kwargs):
+        try:
+            post = Post.objects.get(id=kwargs['post'])
+            like, created = Like.objects.get_or_create(post=post, user=request.user)
+            if created:
+                like.save()
+            serializer = LikeSerializer(like)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request):
-        pass
+
+    def delete(self, request, **kwargs):
+        post = Post.objects.get(id=kwargs['post'])
+        Like.objects.get(user=request.user, post=post).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
